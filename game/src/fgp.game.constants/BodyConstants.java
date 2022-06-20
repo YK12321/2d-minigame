74344e9fe13b37c3571a3b2afc2cda91ddc88132
YK12321 <99835099+YK12321@users.noreply.github.com>
2022-06-20 10:58:35 -0700

    Create BodyConstants.java

---
+++ game/src/fgp.game.constants/BodyConstants.java
@@ -0,0 +1,49 @@
+package fgp.game.constants;
+
+/**
+ * TODO #3 Make one char for each Body that you create. This will let us show
+ * the graphics, and do Collision detection. Write a short comment before each
+ * one, so you know what it represents.
+ * 
+ * @author Mr. Hapke
+ */
+public abstract class BodyConstants {
+	// empty space
+	public static final char S = '-';
+	// Mario_1
+	public static final char M = 'M';
+	// Grass_Living
+	public static final char G = 'G';
+	// Steve
+	public static final char P = 'S';
+	// Goomba
+	public static final char B = 'B';
+	// Glitched Goomba
+	public static final char Q = 'Q';
+	// Exit portal
+	public static final char E = 'E';
+	// Lucky block
+	public static final char L = 'L';
+	// Upgraded Sheild Powerup
+	public static final char K = 'K';
+	// Shield Powerup
+	public static final char Z = 'Z';
+	// Door
+	public static final char D = 'D';
+	// BackgroundImage2
+	public static final char W = 'W';
+	// Asteroid
+	public static final char R = 'R';
+	// Lava
+	public static final char T = 'T';
+	// Glitched projectile
+	public static final char Y = 'Y';
+	// Pen
+	public static final char U = 'U';
+	// Pen's Mid attack
+	public static final char I = 'I';
+	// Pen' Lightning attack
+	public static final char O = 'O';
+	// Elytra Powerup
+	public static final char F = 'F';
+;}
