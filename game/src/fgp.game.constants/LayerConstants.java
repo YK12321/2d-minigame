8b9706a72f1b57981c8f730e7b2766772d5e4791
YK12321 <99835099+YK12321@users.noreply.github.com>
2022-06-20 10:59:07 -0700

    Create LayerConstants.java

---
+++ game/src/fgp.game.constants/LayerConstants.java
@@ -0,0 +1,11 @@
+package fgp.game.constants;
+
+/**
+ * @author Mr. Hapke
+ */
+public abstract class LayerConstants {
+	public static int backgroundImageLayerId = 0;
+	public static int backgroundLayerId = 0;
+	public static int charactersLayerId = 0;
+	public static int statusLayerId = 0;
+}
