1084ad826e9b1b52c20e55a9ca3cfa692d45284a
YK12321 <99835099+YK12321@users.noreply.github.com>
2022-06-20 10:42:02 -0700

    Create LuckyBlock.java

---
+++ game/src/fgp.game.bodies/LuckyBlock.java
@@ -0,0 +1,34 @@
+package fgp.game.bodies;
+
+import fgp.engine.Body;
+import fgp.engine.GameEngine;
+import fgp.engine.Layer;
+import fgp.game.constants.BodyConstants;
+
+public class LuckyBlock extends Body {
+
+	public LuckyBlock(GameEngine g, Layer l, int x, int y) {
+		super(g, l, x, y);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	protected int getAdvanceDelay() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	protected boolean collision(Body b, int dx, int dy) {
+		// TODO Auto-generated method stub
+		
+		return false;
+	}
+
+	@Override
+	public char getSpriteId() {
+		// TODO Auto-generated method stub
+		return BodyConstants.L;
+	}
+
+}
